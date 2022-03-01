44343fcd065c5d255864c3b509d675c3bbb58720
BuildTools <unconfigured@null.spigotmc.org>
2022-03-01 08:35:01 +0100

    added Database

---
+++ Aldrian_Database.sql
@@ -0,0 +1,40 @@
+create database if not exists Sportverein;
+use Sportverein;
+
+create table if not exists Radrennen
+(
+	RennenID int auto_increment unique key primary key,
+    Land text,
+    Titel varchar(64),
+    Datum date,
+    LängeInKm int
+);
+
+create table if not exists Hauptsponsor
+(
+    SponsorID int auto_increment unique key primary key,
+    Name varchar(64),
+    Sponsorbetrag int,
+    Werbungsart text,
+    Land varchar(64)
+);
+
+create table if not exists Sportler
+(
+    SportlerID int auto_increment unique key primary key,
+    Land varchar(64),
+    Vorname varchar(64),
+    Nachname varchar(64),
+    Radmarke varchar(128)
+);
+
+create table if not exists Sportler_Radrennen
+(
+    Sportler_Radrennen_ID int auto_increment unique key primary key,
+    SportlerID int,
+    RennenID int,
+    Best_Zeit timestamp
+);
+
+alter table Sportler_Radrennen add constraint SportlerID foreign key (SportlerID) references Sportler(SportlerID);
+alter table Sportler_Radrennen add constraint RennenID foreign key (RennenID) references Radrennen(RennenID);
