dd430047e17c1770d94eeab7ad8397064059c08c
BuildTools <unconfigured@null.spigotmc.org>
2022-03-01 16:02:13 +0100

    modified app.py, base.html

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, redirect, request, flash, session
 from flask.templating import render_template
 
+from db.model import db
+
 app = Flask(__name__)
 app.secret_key = "VerySecretSecretKey"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/Sportverein"
+db.init_app(app)
+
+
+#app routes
+
+app.run(debug=True)
