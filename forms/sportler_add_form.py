7cc11985f1896e5fd9f0a6026a23749d436af867
BuildTools <unconfigured@null.spigotmc.org>
2022-03-15 08:37:52 +0100

    added forms folder and sportler added

---
+++ forms/sportler_add_form.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms.fields.datetime import DateField
+from wtforms.fields.simple import BooleanField, StringField, TextAreaField
+from wtforms.fields import DecimalField
+from wtforms import validators
+
+class AddProductForm(FlaskForm):
+    Vorname = StringField("Vorname")
+    Nachname = StringField("Nachname")
+    Land = StringField("Land")
+    Radmarke = StringField("Radmarke")
