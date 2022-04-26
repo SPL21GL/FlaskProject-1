f8a6d26211443539544a2dcf07ed3afc2c9c269c
BubuBiB00 <florentin.aldrian@ms.bulme.at>
2022-04-26 09:52:16 +0200

    removed index

--- app.py
+++ app.py
@@ -2,9 +2,9 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from controllers.index import index_blueprint
-from controllers.Radrennen import radrennen_blueprint
-from controllers.Sportler import sportler_blueprint
-from controllers.Hauptsponsor import hauptsponsor_blueprint
+from controllers.radrennen import radrennen_blueprint
+from controllers.sportler import sportler_blueprint
+from controllers.hauptsponsor import hauptsponsor_blueprint
 from db.model import db
 
 app = Flask(__name__)
@@ -16,7 +16,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/Sportverein
 
 db.init_app(app)
 
-app.register_blueprint(index_blueprint)
 app.register_blueprint(hauptsponsor_blueprint)
 app.register_blueprint(sportler_blueprint)
 app.register_blueprint(radrennen_blueprint)
